40b58720d24f1b5dabee4a712392a99bf58f909e
KarolisJasadKK <karolisjasad@gmail.com>
2023-06-29 10:29:55 +0300

    Class info menu with emojis

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
@@ -13,6 +12,11 @@ intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+class SimpleView(discord.ui.View):
+    @discord.ui.button(label="Hello", style=discord.ButtonStyle.blurple)
+    async def hello(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("World")
 
 @bot.event
 async def on_ready():
@@ -25,71 +29,95 @@ async def on_ready():
     print("Member list:")
     for member in guild.members:
         print(member.name)
-    
-    await general_channel.send("I'm online now!")
-    
 
-pages = [
-    {
-        'title': "Option 1",
-        'description': "This is option 1. Select this to see more information.",
-        'details': "More details about option 1."
-    },
-    {
-        'title': "Option 2",
-        'description': "This is option 2. Select this to see more information.",
-        'details': "More details about option 2."
-    },
-    {
-        'title': "Option 3",
-        'description': "This is option 3. Select this to see more information.",
-        'details': "More details about option 3."
-    }
-]
+    await general_channel.send("I'm online now!")
 
 @bot.command()
-async def info(ctx):
-    embed = discord.Embed(title="Information Menu", description=pages[0]['description'])
-    embed.set_footer(text=f"Page 1/{len(pages)}")
+async def button(ctx):
+    view = SimpleView()
+    await ctx.send(view=view)
 
-    message = await ctx.send(embed=embed)
+@bot.command()
+async def startrpg(ctx):
+    warrior_page = discord.Embed(
+        title="Warrior Class",
+        description="A powerful class specializing in melee combat.",
+        color=discord.Color.blue()
+    )
+    warrior_page.add_field(name="Stats", value="HP: 100\nAttack: 80\nDefense: 70\nMagic: 20")
 
+    rogue_page = discord.Embed(
+        title="Rogue Class",
+        description="A swift and stealthy class adept at sneaky maneuvers.",
+        color=discord.Color.green()
+    )
+    rogue_page.add_field(name="Stats", value="HP: 70\nAttack: 60\nDefense: 50\nMagic: 40")
+
+    mage_page = discord.Embed(
+        title="Mage Class",
+        description="A spellcasting class with powerful magical abilities.",
+        color=discord.Color.purple()
+    )
+    mage_page.add_field(name="Stats", value="HP: 50\nAttack: 40\nDefense: 30\nMagic: 100")
+
+    class_embeds = [warrior_page, rogue_page, mage_page]
+
+    message = await ctx.send(embed=class_embeds[0])
+    page_index = 0
+
+    # Add reactions for navigation
     await message.add_reaction('⬅️')
-    await message.add_reaction('1️⃣')
     await message.add_reaction('➡️')
-    await message.add_reaction('🔍')
 
     def check(reaction, user):
-        return user == ctx.author and str(reaction.emoji) in ['⬅️', '1️⃣', '➡️', '🔍']
-
-    current_page = 0
+        return user == ctx.author and str(reaction.emoji) in ['⬅️', '➡️']
 
     while True:
         try:
             reaction, _ = await bot.wait_for('reaction_add', timeout=60.0, check=check)
-            if str(reaction.emoji) == '⬅️':
-                current_page = (current_page - 1) % len(pages)
-            elif str(reaction.emoji) == '1️⃣':
-                current_page = 0
-            elif str(reaction.emoji) == '➡️':
-                current_page = (current_page + 1) % len(pages)
-            elif str(reaction.emoji) == '🔍':
-                await message.delete()
-                await show_details(ctx, pages[current_page])
 
-                # Exit the loop after showing the details
-                break
+            if str(reaction.emoji) == '➡️':
+                page_index = (page_index + 1) % len(class_embeds)
+            elif str(reaction.emoji) == '⬅️':
+                page_index = (page_index - 1) % len(class_embeds)
 
-            embed.description = pages[current_page]['description']
-            embed.set_footer(text=f"Page {current_page+1}/{len(pages)}")
-            await message.edit(embed=embed)
+            await message.edit(embed=class_embeds[page_index])
             await message.remove_reaction(reaction, ctx.author)
+
         except TimeoutError:
+            await message.delete()
             break
 
-async def show_details(ctx, page):
-    embed = discord.Embed(title=page['title'], description=page['details'])
-    await ctx.send(embed=embed)
+    select_button = discord.ui.Button(style=discord.ButtonStyle.primary, label="Select Class")
 
+    class_selection_view = discord.ui.View()
+    class_selection_view.add_item(select_button)
+
+    class_selection_message = await ctx.send("Select your class:", view=class_selection_view)
+
+    def check_class_selection(interaction):
+        return interaction.user == ctx.author and interaction.message.id == class_selection_message.id
+
+    try:
+        interaction = await bot.wait_for("button_click", check=check_class_selection, timeout=60.0)
+        selected_class = None
+
+        if interaction.component.label == "Select Class":
+            if page_index == 0:
+                selected_class = "Warrior"
+            elif page_index == 1:
+                selected_class = "Rogue"
+            elif page_index == 2:
+                selected_class = "Mage"
+
+        await interaction.response.send_message(f"You have selected {selected_class} class.")
+
+    except TimeoutError:
+        await class_selection_message.delete()
+        await ctx.send("Class selection timed out.", delete_after=10)
+        return
+
+    await ctx.send(f"Selected class: {selected_class}")
+    # Here you can continue with the logic for the selected class
 
 bot.run(TOKEN)
