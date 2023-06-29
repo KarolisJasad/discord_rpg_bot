f71c5820db9086a50a47ee06fc8a523c8295ec82
Mindaugas Turauskas <min.turauskas@gmail.com>
2023-06-29 16:45:07 +0300

    Added images to character_class

--- rpgbot_project/cogs/classmenu.py
+++ rpgbot_project/cogs/classmenu.py
@@ -20,17 +20,15 @@ class ClassMenu(commands.Cog):
         )
         warrior_page.add_field(name="Warrior", value="A powerful class specializing in melee combat.")
         warrior_page.add_field(name="Stats", value="HP: 100\nAttack: 80\nDefense: 70\nMagic: 20")
-        warrior_image = discord.File("media/class_images/Warrior.jpg", filename="Warrior.jpg")
-        warrior_page.set_image(url="attachment://Warrior.jpg")
-        
+        warrior_page.set_image(url="https://i.imgur.com/DgzRO8S.png")
+
         rogue_page = discord.Embed(
             title="Choose your class!",
             color=discord.Color.green()
         )
         rogue_page.add_field(name="Rogue", value="A swift and stealthy class adept at sneaky maneuvers.")
         rogue_page.add_field(name="Stats", value="HP: 70\nAttack: 60\nDefense: 50\nMagic: 40")
-        rogue_image = discord.File("media/class_images/Rogue.png", filename="Rogue.png")
-        rogue_page.set_image(url="attachment://Rogue.png")
+        rogue_page.set_image(url="https://i.imgur.com/AffR54p.png")
 
         mage_page = discord.Embed(
             title="Choose your class!",
@@ -38,15 +36,13 @@ class ClassMenu(commands.Cog):
         )
         mage_page.add_field(name="Mage", value="A spellcasting class with powerful magical abilities.")
         mage_page.add_field(name="Stats", value="HP: 50\nAttack: 40\nDefense: 30\nMagic: 100")
-        mage_image = discord.File("media/class_images/Mage.png", filename="Mage.png")
-        mage_page.set_image(url="attachment://Mage.png")
+        mage_page.set_image(url="https://i.imgur.com/9hAD0yP.png")
 
         class_embeds = [warrior_page, rogue_page, mage_page]
-        class_images = [warrior_image, rogue_image, mage_image]
 
         def update_class_message():
             return class_embeds[self.page_index]
-        
+
         class_selection_view = discord.ui.View()
         select_button = discord.ui.Button(style=discord.ButtonStyle.primary, label="Select Class")
 
@@ -61,9 +57,11 @@ class ClassMenu(commands.Cog):
 
             await interaction.message.edit(embed=class_embeds[self.page_index])
 
-        previous_button = discord.ui.Button(style=discord.ButtonStyle.secondary, label="Previous", custom_id="previous_button")
+        previous_button = discord.ui.Button(style=discord.ButtonStyle.secondary, label="Previous",
+                                            custom_id="previous_button")
         next_button = discord.ui.Button(style=discord.ButtonStyle.secondary, label="Next", custom_id="next_button")
-        select_button = discord.ui.Button(style=discord.ButtonStyle.primary, label="Select Class", custom_id="select_button")
+        select_button = discord.ui.Button(style=discord.ButtonStyle.primary, label="Select Class",
+                                          custom_id="select_button")
 
         previous_button.callback = on_select_button_click
         next_button.callback = on_select_button_click
@@ -73,7 +71,7 @@ class ClassMenu(commands.Cog):
         class_selection_view.add_item(next_button)
         class_selection_view.add_item(select_button)
 
-        await ctx.send(file=class_images[self.page_index], embed=class_embeds[self.page_index], view=class_selection_view)
+        await ctx.send(embed=class_embeds[self.page_index], view=class_selection_view)
 
     async def handle_class_selection(self, interaction):
         print("Working function")
@@ -93,6 +91,7 @@ class ClassMenu(commands.Cog):
         await player.save()
 
         await interaction.response.send_message(f"You have selected {selected_class} class.")
+
 
 def setup(bot):
     bot.add_cog(ClassMenu(bot))
