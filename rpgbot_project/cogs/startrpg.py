68e564b99438daf386321ee5f02c59b21fac496f
KarolisJasadKK <karolisjasad@gmail.com>
2023-07-05 12:52:49 +0300

    Fixed !startrpg to make new channels/redirecting

--- rpgbot_project/cogs/startrpg.py
+++ rpgbot_project/cogs/startrpg.py
@@ -15,44 +15,50 @@ class UsernameEntry(commands.Cog):
             return m.author == ctx.author
 
         player = await sync_to_async(Player.objects.filter(player_id=ctx.author.id).exists)()
-        channel_name = 'rpg-channelis'
-        existing_channel = discord.utils.get(ctx.guild.channels, name=channel_name)
-        channel_number = 1
-        while existing_channel:
-            channel_number += 1
-            new_channel_name = f'{channel_name}-{channel_number}'
-            existing_channel = discord.utils.get(ctx.guild.channels, name=new_channel_name)
 
-        overwrites = {
-            ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
-            ctx.author: discord.PermissionOverwrite(read_messages=True)
-        }
-        
-        channel = await ctx.guild.create_text_channel(new_channel_name, overwrites=overwrites)
-        ctx.channel = channel  # Update ctx with the new channel
-
-        print(channel.id)
-        def check(message):
-            return message.channel == ctx.channel and not message.author.bot
         if player:
-            # Player entry exists, proceed to the class menu
-            class_menu_command = self.bot.get_command("classmenu")
-            await ctx.invoke(class_menu_command)
-            return
+            # Player entry exists, redirect to the associated channel
+            player = await sync_to_async(Player.objects.get)(player_id=ctx.author.id)
+            channel_name = f"rpg-channel-{player.username.lower()}"
+            existing_channel = discord.utils.get(ctx.guild.channels, name=channel_name)
 
-        await ctx.send("Please enter your username.")
-        try:
-            username_msg = await self.bot.wait_for("message", check=check_author, timeout=60.0)
-            username = username_msg.content
-            await sync_to_async(Player.objects.create)(player_id=ctx.author.id, discord_name=ctx.author.name, username=username)
+            if existing_channel:
+                # Update ctx with the existing channel
+                ctx.channel = existing_channel
+                class_menu_command = self.bot.get_command("classmenu")
+                await ctx.invoke(class_menu_command)
+                return
 
-            # Proceed to the class menu
-            class_menu_command = self.bot.get_command("classmenu")
-            await ctx.invoke(class_menu_command)
+        else:
+            await ctx.send("Please enter your username.")
+            try:
+                username_msg = await self.bot.wait_for("message", check=check_author, timeout=60.0)
+                username = username_msg.content
 
-        except:
-            await ctx.send("Username entry timed out.", delete_after=10)
-            return
+                channel_name = f"RPG-Channel-{username}"
+                existing_channel = discord.utils.get(ctx.guild.channels, name=channel_name)
+
+                if existing_channel:
+                    ctx.channel = existing_channel  # Update ctx with the existing channel
+                else:
+                    overwrites = {
+                        ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+                        ctx.author: discord.PermissionOverwrite(read_messages=True)
+                    }
+                    channel = await ctx.guild.create_text_channel(channel_name, overwrites=overwrites)
+                    ctx.channel = channel  # Update ctx with the new channel
+                    print(channel.id)
+
+                await sync_to_async(Player.objects.create)(player_id=ctx.author.id, discord_name=ctx.author.name, username=username)
+                await ctx.send(f"Welcome, {username}!")
+
+                # Proceed to the class menu
+                class_menu_command = self.bot.get_command("classmenu")
+                await ctx.invoke(class_menu_command)
+
+            except:
+                await ctx.send("Username entry timed out.", delete_after=10)
+                return
 
 def setup(bot):
     bot.add_cog(UsernameEntry(bot))
