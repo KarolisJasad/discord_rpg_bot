b095e1b535eb6fbe2cf48b5de96ed4ae1fac420f
KarolisJasadKK <karolisjasad@gmail.com>
2023-07-17 18:46:04 +0300

    Initial django website

--- rpgbot_project/rpgbot/urls.py
+++ rpgbot_project/rpgbot/urls.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from rpgbot.views import AboutView, LeaderboardView, ItemsView
 
 urlpatterns = [
-
+    path('about/', AboutView.as_view(), name='about'),
+    path('leaderboard/', LeaderboardView.as_view(), name='leaderboard'),
+    path('items/', ItemsView.as_view(), name='items'),
 ]
